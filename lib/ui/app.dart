af58dacb739c08a04120791e1e1bb851d80f74c3
bui danh nam <nambui@gmail.com>
2021-04-17 10:11:18 +0700

    update routeObserver

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,28 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_template/blocs/bloc_observer.dart';
 import 'package:flutter_template/constants.dart';
 import 'package:flutter_template/localizations.dart';
 import 'package:flutter_template/routes.dart';
 import 'package:flutter_template/utils/navigator.dart';
 
-import '../route_observer.dart';
+RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
 class MyApp extends StatelessWidget {
-  final _routeObserver = MyObserver();
-
   final String language;
 
   MyApp.language(this.language);
 
   @override
   Widget build(BuildContext context) {
-    Bloc.observer = MyBlocObserver();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      navigatorObservers: [_routeObserver],
+      navigatorObservers: [routeObserver],
       navigatorKey: NavigationService.instance.navigatorKey,
       localizationsDelegates: [
         const LanguageDelegate(),
@@ -31,12 +26,9 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         DefaultCupertinoLocalizations.delegate
       ],
-      locale: this.language == null
-          ? Constants.SUPPORT_LOCALE[0]
-          : Locale(this.language),
+      locale: this.language == null ? Constants.SUPPORT_LOCALE[0] : Locale(this.language),
       supportedLocales: Constants.SUPPORT_LOCALE,
-      localeResolutionCallback: (locale, supportedLocales) =>
-          _localeCallback(locale, supportedLocales),
+      localeResolutionCallback: (locale, supportedLocales) => _localeCallback(locale, supportedLocales),
       initialRoute: Routes.initScreen(),
       onGenerateRoute: Routes.generateRoute,
     );
