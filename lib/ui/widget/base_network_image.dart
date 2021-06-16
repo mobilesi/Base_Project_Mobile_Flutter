ea3ec46226cef134aab2906872b7ccf1f0a1d519
nambd <nambd@vnpt.vn>
2021-06-16 10:09:41 +0700

    base nettword

---
+++ lib/ui/widget/base_network_image.dart
@@ -0,0 +1,51 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import 'base_progress_indicator.dart';
+
+class BaseNetworkImage extends StatelessWidget {
+  final String url;
+  final double borderRadius;
+  final double width;
+  final double height;
+  final String errorAssetImage;
+  final double loadingSize;
+
+  const BaseNetworkImage(
+      {Key key, this.url, this.borderRadius = 0, this.width, this.height, this.errorAssetImage, this.loadingSize})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget errorWidget = ClipRRect(
+      borderRadius: BorderRadius.circular(borderRadius ?? 10),
+      child: Container(
+        width: this.width ?? double.infinity,
+        height: this.height ?? double.infinity,
+        child: errorAssetImage?.isNotEmpty ?? false
+            ? Image.asset(
+                errorAssetImage,
+                width: this.width,
+                height: this.height,
+                fit: BoxFit.cover,
+              )
+            : Icon(Icons.error),
+      ),
+    );
+    return url == null
+        ? errorWidget
+        : ClipRRect(
+            child: CachedNetworkImage(
+              width: this.width ?? double.infinity,
+              height: this.height ?? double.infinity,
+              fit: BoxFit.cover,
+              imageUrl: url,
+              placeholder: (context, url) => Center(
+                child: BaseProgressIndicator(size: loadingSize),
+              ),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+            borderRadius: BorderRadius.circular(borderRadius ?? 10),
+          );
+  }
+}
---
+++ lib/ui/widget/base_progress_indicator.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/res/colors.dart';
+
+class BaseProgressIndicator extends StatelessWidget {
+  final double size;
+
+  const BaseProgressIndicator({Key key, this.size}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final loading = CircularProgressIndicator(
+      strokeWidth: 3,
+      backgroundColor: AppColors.base_color,
+      valueColor: new AlwaysStoppedAnimation<Color>(AppColors.base_color_border_textfield),
+    );
+    return size == null
+        ? loading
+        : SizedBox(
+            width: size,
+            height: size,
+            child: loading,
+          );
+  }
+}
